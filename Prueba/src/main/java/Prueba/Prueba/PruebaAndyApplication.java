5af4a8df9c529fff2a58b42df2d6e3d80c4ee9f4
AndyHR14 <andypipoh@gmail.com>
2025-01-29 17:44:33 -0600

    Commit para probar
    A ver q tal

---
+++ Prueba/src/main/java/Prueba/Prueba/PruebaAndyApplication.java
@@ -0,0 +1,13 @@
+package Prueba.Prueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaAndyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaAndyApplication.class, args);
+	}
+
+}
